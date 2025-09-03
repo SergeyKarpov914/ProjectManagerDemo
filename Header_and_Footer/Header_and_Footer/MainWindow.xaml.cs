a44d53fede67d61864e978272974a2fe5e69aade
Sergey Karpov <cliovox0409@gmail.com>
2025-09-03 16:49:22 -0400

    skip: wpf tester

---
+++ Header_and_Footer/Header_and_Footer/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Syncfusion.SfSkinManager;
+using System.Windows;
+
+namespace Header_and_Footer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+     //       SfSkinManager.SetTheme(this, new Theme("Material3Light"));
+        }
+    }
+}
--- ProjectManager.WPF/MainWindow.xaml.cs
+++ ProjectManager.WPF/MainWindow.xaml.cs
@@ -25,5 +25,33 @@ namespace Clio.ProjectManagerDemo.WPF
         {
             InitializeComponent();
         }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            Contents = new List<Model>();
+
+            Contents.Add(new Model() { Name = "Home" });
+            Contents.Add(new Model() { Name = "Profile" });
+            Contents.Add(new Model() { Name = "Inbox" });
+            Contents.Add(new Model() { Name = "Outbox" });
+            Contents.Add(new Model() { Name = "Sent" });
+            Contents.Add(new Model() { Name = "Trash" });
+            Contents.Add(new Model() { Name = "Sign Out" });
+        }
+
+        public List<Model> Contents { get; set; }
+    }
+
+    public class Model
+    {
+        public string Name { get; set; }
     }
 }
